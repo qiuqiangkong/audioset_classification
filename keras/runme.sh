9c7f4b3d608d1e8b02a1a5559e1683e7cb07ff32
qiuqiangkong <qiuqiangkong@gmail.com>
2018-04-12 15:34:28 +0100

    delete unrelated

--- keras/runme.sh
+++
@@ -1,2 +0,0 @@
-CUDA_VISIBLE_DEVICES=1 python keras/main.py --data_dir=$DATA_DIR --workspace=$WORKSPACE --mini_data --balance_type=balance_in_batch --model_type=feature_level_attention train
-
--- keras/test9.py
+++
@@ -1,4 +0,0 @@
-import os
-import sys
-os.path.join(sys.path[0], '../')
-print(sys.path)
